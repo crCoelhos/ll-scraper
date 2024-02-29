b84dffc2bee7127f0ae7cb08d7b01077a42daf55
crCoelhosm <cristophersaar@gmail.com>
2024-02-29 14:19:47 -0500

    Add new routes for process/version and legaliga api

--- routes/crowlRoutes.js
+++ routes/crowlRoutes.js
@@ -5,6 +5,19 @@ import main from '../controllers/mainCrowlController.js';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+    res.send('process/version is running');
+});
+
+router.get('/main/', (req, res) => {
+    res.send('process/version/main is running');
+});
+
+app.get('/', (req, res) => {
+    res.send('legaliga api running');
+});
+
+
 router.get('/search/:keyword', crowl.search);
 router.get('/main/:keyword', main.search);
 
